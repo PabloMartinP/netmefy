bd2fa603ef5b820250c8bc3b2097cc77b2c339b2
Ignacio Döring <ignaciodoring@gmail.com>
2017-06-15 03:14:17 -0300

    se traen los primeros 25 likes de la persona

--- NetmefyApp/app/src/main/java/ar/com/netmefy/netmefy/LoginActivity.java
+++ NetmefyApp/app/src/main/java/ar/com/netmefy/netmefy/LoginActivity.java
@@ -1,17 +1,17 @@
 package ar.com.netmefy.netmefy;
 
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.graphics.Color;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.KeyEvent;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.facebook.AccessToken;
 import com.facebook.AccessTokenTracker;
@@ -19,233 +19,127 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.LoggingBehavior;
 import com.facebook.Profile;
 import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
-import ar.com.netmefy.netmefy.cliente.ClienteActivity;
-import ar.com.netmefy.netmefy.tecnico.TecnicoActivity;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 
 public class LoginActivity extends AppCompatActivity {
-    //private UserLoginTask _authTask = null;
-
-    /*private EditText _idView;
-    private EditText _passwordView;*/
-    private ProgressDialog _progressBar;
-
     private CallbackManager callbackManager;
-    private AccessTokenTracker accessTokenTracker;
     private ProfileTracker profileTracker;
-
-    //Facebook login button
-    private FacebookCallback<LoginResult> callback = new FacebookCallback<LoginResult>() {
-        @Override
-        public void onSuccess(LoginResult loginResult) {
-            AccessToken accessToken = loginResult.getAccessToken();
-            Profile profile = Profile.getCurrentProfile();
-            Toast.makeText(getApplicationContext(), "Logging in...", Toast.LENGTH_SHORT).show();
-            nextActivity(profile);
-        }
-        @Override
-        public void onCancel() {        }
-        @Override
-        public void onError(FacebookException e) {      }
-    };
+    private LoginButton loginButton;
+    private String firstName,lastName, email,birthday,gender, next;
+    private JSONObject likes, paging;
+    private JSONArray likesNamesJson;
+    private ArrayList<String>likesNames;
+    private URL profilePicture;
+    private String userId;
+    private String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this);
         setContentView(R.layout.activity_login);
-        /*_idView = (EditText) findViewById(R.id.et_email);
-        _passwordView = (EditText) findViewById(R.id.password);
 
-        //evento enter
-        _passwordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                boolean handled = false;
-                if (actionId == EditorInfo.IME_ACTION_SEND) {
-                    login();
-                    handled = true;
-                }
-                return handled;
-            }
-        });
-
-
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                login();
-            }
-        });
-
-        inicializarProgressBar(this);*/
-
-        FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
-        accessTokenTracker = new AccessTokenTracker() {
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setHeight(100);
+        loginButton.setTextColor(Color.WHITE);
+        loginButton.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
+        loginButton.setCompoundDrawablePadding(0);
+
+
+        FacebookCallback<LoginResult> callback = new FacebookCallback<LoginResult>() {
             @Override
-            protected void onCurrentAccessTokenChanged(AccessToken oldToken, AccessToken newToken) {
+            public void onSuccess(LoginResult loginResult) {
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        Log.e(TAG,object.toString());
+                        Log.e(TAG,response.toString());
+
+                        try {
+                            userId = object.getString("id");
+                            profilePicture = new URL("https://graph.facebook.com/" + userId + "/picture?width=500&height=500");
+                            if(object.has("first_name"))
+                                firstName = object.getString("first_name");
+                            if(object.has("last_name"))
+                                lastName = object.getString("last_name");
+                            if (object.has("email"))
+                                email = object.getString("email");
+                            if (object.has("birthday"))
+                                birthday = object.getString("birthday");
+                            if (object.has("gender"))
+                                gender = object.getString("gender");
+                            if (object.has("likes")) {
+                                likes = (JSONObject)object.get("likes");
+                                likesNamesJson = (JSONArray)likes.get("data");
+                                likesNames = new ArrayList<String>();
+                                for(int i = 0; i < likesNamesJson.length(); i++){
+                                    likesNames.add(((JSONObject)likesNamesJson.get(i)).getString("name"));
+                                }
+                                paging = (JSONObject)likes.get("paging");
+                                next = paging.getString("next");
+                            }
+
+                            Intent main = new Intent(LoginActivity.this,MainActivity.class);
+                            main.putExtra("name",firstName);
+                            main.putExtra("surname",lastName);
+                            main.putExtra("imageUrl",profilePicture.toString());
+                            main.putStringArrayListExtra("likesNames", likesNames);
+                            startActivity(main);
+                            finish();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id, first_name, last_name, email, birthday, gender , location, likes");
+                parameters.putString("limit","400");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                e.printStackTrace();
             }
         };
 
-        profileTracker = new ProfileTracker() {
-            @Override
-            protected void onCurrentProfileChanged(Profile oldProfile, Profile newProfile) {
-                nextActivity(newProfile);
-            }
-        };
-        accessTokenTracker.startTracking();
-        profileTracker.startTracking();
-
-        LoginButton loginButton = (LoginButton)findViewById(R.id.login_button);
-        loginButton.setReadPermissions("user_friends");
+        loginButton.setReadPermissions("email", "user_birthday","user_posts", "user_likes");
         loginButton.registerCallback(callbackManager, callback);
-
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        //Facebook login
-        Profile profile = Profile.getCurrentProfile();
-        nextActivity(profile);
-    }
-
-    @Override
-    protected void onPause() {
-
-        super.onPause();
-    }
-
-    protected void onStop() {
-        super.onStop();
-        //Facebook login
-        accessTokenTracker.stopTracking();
-        profileTracker.stopTracking();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
         super.onActivityResult(requestCode, responseCode, intent);
-        //Facebook login
         callbackManager.onActivityResult(requestCode, responseCode, intent);
-
     }
 
-    private void nextActivity(Profile profile){
-        if(profile != null){
-            Intent main = new Intent(LoginActivity.this, MainActivity.class);
-            main.putExtra("name", profile.getFirstName());
-            main.putExtra("surname", profile.getLastName());
-            main.putExtra("imageUrl", profile.getProfilePictureUri(200,200).toString());
-            startActivity(main);
-        }
-    }
-
-    /*private void login() {
-        // Store values at the time of the login attempt.
-//        String email = _idView.getText().toString();
-//        String password = _passwordView.getText().toString();
-
-        boolean cancel = false;
-        if (cancel) {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
-        } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            showProgress(true);
-            _authTask = new UserLoginTask(email, password, this.getApplicationContext());
-            _authTask.execute((Void) null);
-
-        }
-    }
-
-    private void showProgress(boolean b){
-        if(b) {
-            _progressBar.show();
-            //Toast.makeText(this.getApplicationContext(), "entrando toast", Toast.LENGTH_LONG).show();
-        }
-        else {
-            _progressBar.dismiss();
-        }
-    }
-
-    private void inicializarProgressBar(LoginActivity loginActivity) {
-        _progressBar = new ProgressDialog(this);
-        _progressBar.setMessage(getString(R.string.login_ingresando));
-        _progressBar.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        _progressBar.setCancelable(false);
-    }*/
-
-    /*public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-        private Boolean _esCliente = true;
-        private final String _id;
-        private final String _password;
-        private  final Context _context;
-        @Override
-        protected void onPreExecute(){
-            showProgress(true);
-        }
-
-
-        UserLoginTask(String email, String password, Context context ) {
-            _id = email;
-            _password = password;
-            _context = context;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-
-            try {
-                // Simulate network access.
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            _esCliente = _id.length() == 0;
-
-            // TODO: register the new account here.
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            _authTask = null;
-
-            if (success) {
-                Intent intent;
-                if(_esCliente){
-                    intent = new Intent( LoginActivity.this, ClienteActivity.class);
-                    Toast.makeText(_context, "Alto cliente", Toast.LENGTH_SHORT).show();
-                }else{
-                    intent = new Intent( LoginActivity.this, TecnicoActivity.class);
-                    Toast.makeText(_context, "Tecnico de la cablera", Toast.LENGTH_SHORT).show();
-                }
-
-                showProgress(false);
-                startActivity(intent);
-                finish();
-            } else {
-                Toast.makeText(_context, R.string.error_login, Toast.LENGTH_SHORT).show();
-                //_passwordView.requestFocus();
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            _authTask = null;
-            showProgress(false);
-        }
-    }*/
 }
--- NetmefyApp/app/src/main/java/ar/com/netmefy/netmefy/MainActivity.java
+++ NetmefyApp/app/src/main/java/ar/com/netmefy/netmefy/MainActivity.java
@@ -4,12 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginManager;
+
+import java.util.ArrayList;
 
 import ar.com.netmefy.netmefy.services.DownloadImage;
 
@@ -17,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView nameView;
     private Button logout;
+    private ListView lvLikes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +31,20 @@ public class MainActivity extends AppCompatActivity {
         String name = inBundle.get("name").toString();
         String surname = inBundle.get("surname").toString();
         String imageUrl = inBundle.get("imageUrl").toString();
+        ArrayList<String> likesNames = inBundle.getStringArrayList("likesNames");
         FacebookSdk.sdkInitialize(getApplicationContext());
         nameView = (TextView)findViewById(R.id.nameAndSurname);
         nameView.setText("" + name + " " + surname);
         new DownloadImage((ImageView)findViewById(R.id.profileImage)).execute(imageUrl);
 
+        lvLikes = (ListView) findViewById(R.id.lv_likes);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                likesNames );
+
+        lvLikes.setAdapter(arrayAdapter);
         logout = (Button)findViewById(R.id.logout);
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
